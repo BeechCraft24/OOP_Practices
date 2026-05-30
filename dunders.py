9d63ca999ad94c85acdb5449bb73e7d7e6cbef45
alexander <your-GitHub-ID@users.noreply.github.com>
2026-05-30 15:23:57 -0400

    created new commit 5/30/2026

---
+++ dunders.py
@@ -0,0 +1,31 @@
+# number = 10
+# print(number + 20)
+
+
+# number = 10
+# print(number.__add__(20))
+
+
+# class Box:
+#     def __init__(self, value):
+#         self.value = value
+#     def __add__(self, other):
+#         return self.value + other.value   
+# a = Box(10)
+# b = Box(20)
+# print(a + b)
+
+
+class Person:
+    def __init__(self, weight, age, salary):
+        self.weight = weight
+        self.age = age
+        self.salary = salary
+
+    def __add__(self, other):
+        return self.age + other.salary
+
+p1 = Person(30, 40, 50)
+p2 = Person(35, 45, 55)
+print(p1 + p2)
+
---
+++ import random.py
@@ -0,0 +1,7 @@
+import random
+
+numbers = []
+for i in range(5):
+    x = random.uniform(0.2, 0.5)
+    numbers.append(x)
+print(numbers)
